4f90348162287cb3c61d6993bcab50dc122a14d9
Tech With Hasiya <90503467+HasiyaTechlk@users.noreply.github.com>
2021-09-13 13:15:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,31 @@
-import os
-import telebot
+from pyrogram import Client, filters
 
-bot = telebot. TeleBot ("1947857384:AAHSCGN39-e7syB34pezap-gP5TVDgm086w")
+API_ID = 7407598
+API_HASH = '6e6622a5e352ee0c86cc558ca1c17de3'
+BOT_TOKEN = '1972230569:AAH1gxeB2oVnbcyY_n6cn2iwjVPwc_FLsdU'
 
-@bot.message_handler("commands=["start"])
-def send welcome(message):
-    bot.reply_to(message,"Hello i'm Hasiya Chat Bot")
+bot = Client("Hasiya Bot",
+             api_hash=API_HASH,
+             api_id=API_ID,
+             bot_token=BOT_TOKEN)
 
-@bot.message_handler(commands=["how"])
-def send_message(message):
-    bot.send_message(message,"https://google.com")
-
-@bot.message_handler(commands=["hasiya"])
-def send_message(message:)
-    bot.send_message(message,"♻️ COVID 19 UPDATES BOT ♻️ - @Covid_19_Updates_SL_Bot
-
-ᴘʜᴏᴛᴏ ᴇᴅɪᴛᴏʀ ʟᴋ - @Photo_Editor_LK_Bot
-
-ᴛᴇᴄʜ ᴡɪᴅᴇ ʙᴏᴛ ᴘᴏᴡᴇʀᴅ ʙʏ ʜᴀsɪʏᴀ - @Hasiya_Tech_Bot
-
-HASIYA MUSIC - @Music_Downloder_By_Hasiya_Bot
-
-All Bots Made By - @HASIYA_GEEK
-😉 Made BY Hasiya 😉")
-    bot.polling()
+@bot.on_message(filters.command(commands=['hasiya']))
+async def welcome(client, message):
+    await bot.send_message(chat_id=message.chat.id, text="Hi...")
     
-
-
+@bot.on_message(filters.command(commands=['help']))
+async def help(client, message):
+    await bot.send_message(chat_id=message.chat.id, text="Just send me some messages and enjoy")
+    
+@bot.on_message(filters.command(commands=['bot']))
+async def help(client, message):
+    await bot.send_message(chat_id=message.chat.id, text="Hi How are you")
     
     
+@bot.on_message(filters.command(commands=['You Tube']))
+async def help(client, message):
+    await bot.send_message(chat_id=message.chat.id, text="Gaming Hasiya You Tube Channel Link.\n https://www.youtube.com/c/GamingHasiyaYouTube")
+    
+    
+bot.run() 
+
